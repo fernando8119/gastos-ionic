4913f5a1b15476d8ed79d76505341168f7301e9f
jgascona <josegascon@quibim.com>
2024-02-03 13:37:55 +0100

    ajusta columnas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Peraltada', url: '/folder/favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
